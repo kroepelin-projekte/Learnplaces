b51329344860a4e3f29056c433773c3db156733d
Nicolas Schaefli <nick@fluxlabs.ch>
2021-10-20 18:01:49 +0200

    Fix plugin version constraint

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 $id = "xsrl";
 $version = "4.0.0";
-$ilias_min_version = "6.99";
+$ilias_min_version = "6.0";
 $ilias_max_version = "7.99";
 $responsible = "fluxlabs AG";
 $responsible_mail = "support@fluxlabs.ch";
