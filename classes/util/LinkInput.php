89b057c7e34dd2e494bcd219e0746f01cb30f641
christof.buch <christof.buch@kroepelin-projekte.de>
2024-07-29 17:55:48 +0200

    Ilias link input

--- classes/util/LinkInput.php
+++ classes/util/LinkInput.php
@@ -6,6 +6,7 @@ namespace SRAG\Learnplaces\util;
 
 use ILIAS\UI\Component\Tree\TreeRecursion;
 use ILIAS\UI\Implementation\Component\Input\Field\Numeric;
+use ilObject;
 use ilTree;
 
 class LinkInput
@@ -50,14 +51,25 @@ class LinkInput
     {
         $field = $this->factory->input()->field();
 
+        $ref_id = $default_value;
+        $obj_id = ilObject::_lookupObjId($ref_id);
+        $title = ilObject::_lookupTitle($obj_id);
+        $description = ilObject::_lookupDescription($obj_id);
+
         return $field->numeric($label)
             ->withByline($this->linkPickerModal())
             ->withValue($default_value)
-            ->withOnLoadCode(function ($id) {
+            ->withOnLoadCode(function ($id) use ($ref_id, $title, $description) {
                 return <<<JS
                 (function() {
                     const el = document.getElementById('$id');
                     el.id = 'link_input_element';
+                    el.style.visibility = 'hidden';
+                    const info = document.createElement('div');
+                    info.style.marginTop = '20px';
+                    info.id = 'ilias_link_info';
+                    el.parentElement.appendChild(info);
+                    info.innerHTML = '<b>$title</b><br>Ref ID: $ref_id<br><br>$description';
                 })();
                 JS;
             })
@@ -66,9 +78,9 @@ class LinkInput
 
     private function linkPickerModal(): string
     {
-        $content = $this->expandableTree();
+        $content = $this->expandableTree() . "<div id='ilias_link_info' style='margin-top: 20px;'></div>";
 
-        $modal = $this->factory->modal()->roundtrip('ILIAS Link', $this->factory->legacy($content));
+        $modal = $this->factory->modal()->roundtrip('ILIAS Link', $this->factory->legacy($content))->withCancelButtonLabel('ok');
         $button1 = $this->factory->button()->standard('ILIAS Link', '#')
             ->withOnClick($modal->getShowSignal());
 
@@ -105,13 +117,21 @@ class LinkInput
                 $icon = $environment['icon_factory']->standard($record["type"], '');
                 $url = $this->getAsyncURL($environment, $ref_id);
 
+                $obj_id = ilObject::_lookupObjId((int) $ref_id);
+                $title = ilObject::_lookupTitle($obj_id);
+                $description = ilObject::_lookupDescription($obj_id);
+
                 $node = $factory->simple($label, $icon)
-                    ->withOnLoadCode(function ($id) use ($ref_id) {
+                    ->withOnLoadCode(function ($id) use ($ref_id, $title, $description) {
                         return <<<JS
                         (function() {
                             const node = document.getElementById('$id');
                             const node_label = node.querySelector('.node-label');
                             node_label.addEventListener('click', () => {
+                                const ilias_link_info_elements = Array.from(document.querySelectorAll('#ilias_link_info'));
+                                ilias_link_info_elements.forEach(ilias_link_info => {
+                                    ilias_link_info.innerHTML = '<b>$title</b><br>Ref ID: $ref_id<br><br>$description';
+                                });
                                 const link_input_element = document.getElementById('link_input_element');
                                 link_input_element.value = $ref_id;
                             });
