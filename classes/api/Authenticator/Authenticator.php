f1d1834f7a4298448ccb946c6f3a158e331778ec
Keven Clausen <keven.clausen@kroeplin-projekte.de>
2025-03-17 11:16:15 +0100

    Change Access-Controll-Allow-Origin on Sub Domain

--- classes/api/Authenticator/Authenticator.php
+++ classes/api/Authenticator/Authenticator.php
@@ -176,7 +176,7 @@ class Authenticator
     public function httpOptions(): void
     {
         $client_url = Settings::getClientURL() ?: Settings::getBaseUrl();
-        header("Access-Control-Allow-Origin: $client_url");
+        header("Access-Control-Allow-Origin: .".Settings::getBaseUrl());
         header("Access-Control-Allow-Methods: POST, GET, DELETE, OPTIONS");
         header("Access-Control-Allow-Headers: Authorization, Content-Type, X-Requested-With");
         header("Access-Control-Allow-Credentials: true");
