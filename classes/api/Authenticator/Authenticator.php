72f90b9105543061ecf5d441bf06696eab69a765
Keven Clausen <keven.clausen@kroeplin-projekte.de>
2025-03-17 13:45:01 +0100

    Allow Orgin to *

--- classes/api/Authenticator/Authenticator.php
+++ classes/api/Authenticator/Authenticator.php
@@ -191,12 +191,10 @@ class Authenticator
        ini_set('session.use_trans_sid', 0);
 
         if (in_array($http_origin, $allowed_origins)) {
-            header("Access-Control-Allow-Origin: $http_origin");
-        } else {
             http_response_code(403);
             exit;
         }
-
+        header("Access-Control-Allow-Origin: *");
         header("Access-Control-Allow-Credentials: true");
         header("Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS");
         header("Access-Control-Allow-Headers: Authorization, Content-Type, X-Requested-With");
