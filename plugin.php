e4e4215ce8646ef4e1ecdf9df9634751d4725e4f
Keven Clausen <keven.clausen@kroeplin-projekte.de>
2025-04-24 13:32:57 +0200

    update plugin.php max version

--- plugin.php
+++ plugin.php
@@ -2,6 +2,6 @@
 $id = "xsrl";
 $version = "6.00";
 $ilias_min_version = "9.0";
-$ilias_max_version = "10.999";
+$ilias_max_version = "9.999";
 $responsible = "Kröpelin Projekt GmbH";
 $responsible_mail = "info@kroepelin-projekte.de";
