5b115f24076c4bfe0b74d70029a092535088543c
christof.buch <christof.buch@kroepelin-projekte.de>
2025-05-19 10:07:55 +0200

    logging

--- classes/api/Authenticator/Authenticator.php
+++ classes/api/Authenticator/Authenticator.php
@@ -19,16 +19,24 @@ class Authenticator
      */
     public function auth(): bool
     {
+        $logger = \ilLoggerFactory::getLogger('Learnplaces');
+
+        $authorization_server_variable = $_SERVER['HTTP_AUTHORIZATION'] ?? 'HTTP_AUTHORIZATION_not_set';
+        $logger->info('authorization $_SERVER variable: ' . $authorization_server_variable);
+
         $request_headers = array_change_key_case(getallheaders(), CASE_LOWER);
         if (!isset($request_headers['authorization'])) {
+            $logger->error('no authorization header variable found');
             Response::send(401, DEVMODE ? 'no header' : '', ['success' => false]);
             return false;
         }
         $bearer_token = $request_headers['authorization'];
         if (!str_starts_with($bearer_token, 'Bearer ')) {
+            $logger->error('no bearer token found. authorization header: ' . $request_headers['authorization']);
             Response::send(401, DEVMODE ? 'no bearer token' : '', ['success' => false]);
             return false;
         }
+        $logger->info('bearer token found');
         $http_handler = new HTTPHandler();
         $http_handler->setAccessToken(substr($bearer_token, 7));
         $pkce_handler = new PKCEHandler($http_handler);
