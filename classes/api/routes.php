a6cf88de120314543496c11b5ccc0dacb7469a49
christof.buch <christof.buch@kroepelin-projekte.de>
2025-06-03 16:12:12 +0200

    route renaming

--- classes/api/routes.php
+++ classes/api/routes.php
@@ -4,7 +4,7 @@ $this->add('/learnplaceapp/v1/containers', 'KPG\Learnplaces\api\App\v1', 'Contai
 $this->add('/learnplaceapp/v1/containers/:container_ref_id', 'KPG\Learnplaces\api\App\v1', 'Learnplaces@endpoint', 'GET', true);
 
 $this->add('/learnplaceapp/v1/learnplaces/:id', 'KPG\Learnplaces\api\App\v1', 'LearnplacesInfo@endpoint', 'GET', true);
-$this->add('/learnplaceapp/v1/learnplaces/verify-qr-code/:token', 'KPG\Learnplaces\api\App\v1', 'VerifyQRCode@endpoint', 'POST', true);
+$this->add('/learnplaceapp/v1/learnplaces/verifyqrcode/:token', 'KPG\Learnplaces\api\App\v1', 'VerifyQRCode@endpoint', 'POST', true);
 $this->add('/learnplaceapp/v1/resources/:rid', 'KPG\Learnplaces\api\App\v1', 'GetResources@endpoint', 'GET', true);
 
 $this->add('/learnplaceapp/v1/auth', 'KPG\Learnplaces\api\App\v1', 'Auth@endpoint', 'GET');
