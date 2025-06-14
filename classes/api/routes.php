ea95fae051a00f5cfd0e7aefc47fb19d49fb54f1
Keven Clausen <keven.clausen@kroeplin-projekte.de>
2025-06-14 08:34:32 +0200

    Add Token Access in Endpoint Health

--- classes/api/routes.php
+++ classes/api/routes.php
@@ -1,5 +1,5 @@
 <?php
-$this->add('learnplaceapp/v1/health', 'KPG\Learnplaces\api\App\v1', 'Health@endpoint', 'GET', false);
+$this->add('learnplaceapp/v1/health', 'KPG\Learnplaces\api\App\v1', 'Health@endpoint', 'GET', true);
 
 $this->add('learnplaceapp/v1/containers', 'KPG\Learnplaces\api\App\v1', 'Containers@endpoint', 'GET', true);
 
