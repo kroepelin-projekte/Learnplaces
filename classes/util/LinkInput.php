a8f8737d9246ad123d393e6ef401e8b45d88fecd
christof.buch <christof.buch@kroepelin-projekte.de>
2026-01-21 17:48:13 +0100

    LinkInput adjustments for ILIAS 10

--- classes/util/LinkInput.php
+++ classes/util/LinkInput.php
@@ -15,33 +15,25 @@ use ilObject;
 use ilObjUser;
 use ilTree;
 use KPG\Learnplaces\container\PluginContainer;
+use ILIAS\HTTP\Wrapper\ArrayBasedRequestWrapper;
+use ILIAS\DI\RBACServices;
+use ILIAS\UI\Component\Input\Container\Form\Standard;
 
 class LinkInput
 {
-    /** @var ilTree $tree */
-    private object $tree;
-    /** @var \ILIAS\HTTP\Wrapper\ArrayBasedRequestWrapper $query */
-    private object $query;
-    /** @var ilObjUser $user */
-    private object $user;
-    /** @var \ILIAS\DI\RBACServices $rbac */
-    private object $rbac;
-    /** @var ilCtrl $ctrl */
-    protected object $ctrl;
-    /** @var Services $http */
-    protected object $http;
-    /** @var \ILIAS\ResourceStorage\Services $resourceStorage */
-    protected object $resourceStorage;
-    /** @var Factory $factory */
-    protected object $factory;
-    private object $refinery;
-    /** @var Renderer $renderer */
-    protected object $renderer;
-    /** @var \ILIAS\UI\Component\Input\Field\Factory $field */
-    protected object $field;
-    protected \ILIAS\UI\Component\Input\Container\Form\Standard $form;
-    /** @var mixed|null */
-    protected $form_data;
+    private ilTree $tree;
+    private ArrayBasedRequestWrapper $query;
+    private ilObjUser $user;
+    private RBACServices $rbac;
+    protected ilCtrl $ctrl;
+    protected Services $http;
+    protected \ILIAS\ResourceStorage\Services $resourceStorage;
+    protected Factory $factory;
+    private mixed $refinery;
+    protected Renderer $renderer;
+    protected \ILIAS\UI\Component\Input\Field\Factory $field;
+    protected Standard $form;
+    protected mixed $form_data;
 
     private const ALLOWED_TYPES = [
         'cat', // category
@@ -114,7 +106,7 @@ class LinkInput
 
     /**
      * @param string $label
-     * @param int $default_value
+     * @param int    $default_value
      * @return Numeric
      */
     public function getLinkButton(string $label, int $default_value): Numeric
@@ -122,33 +114,31 @@ class LinkInput
         $field = $this->factory->input()->field();
 
         $ref_id = $this->getRefId($default_value);
-        $obj_id = ilObject::_lookupObjId($ref_id);
-        $title = ilObject::_lookupTitle($obj_id);
 
         return $field->numeric($label)
-                     ->withByline($this->expandableTree())
-                     ->withValue($default_value)
-                     ->withAdditionalTransformation(
-                         $this->refinery->custom()->constraint(
-                             fn ($value) => ilObject::_exists($value, true),
-                             'Object not found'
-                         )
-                     )
-                     ->withOnLoadCode(function ($id) use ($ref_id, $title) {
-                         return <<<JS
+            ->withByline($this->expandableTree())
+            ->withValue($default_value)
+            ->withAdditionalTransformation(
+                $this->refinery->custom()->constraint(
+                    fn($value) => ilObject::_exists($value, true),
+                    'Object not found'
+                )
+            )
+            ->withOnLoadCode(function ($id) use ($ref_id) {
+                return <<<JS
                 (function() {
                     const el = document.getElementById('$id');
                     el.id = 'link_input_element';
-                    el.style.visibility = 'hidden';
                     const info = document.createElement('div');
+                    el.querySelector('input').style.pointerEvents = 'none';
                     info.style.marginTop = '20px';
                     info.id = 'ilias_link_info';
                     el.parentElement.prepend(info);
                     info.innerHTML = '<b>ILIAS</b><br>Ref ID: $ref_id<br>';
                 })();
                 JS;
-                     })
-                     ->withRequired(true);
+            })
+            ->withRequired(true);
     }
 
     /**
@@ -164,13 +154,18 @@ class LinkInput
             $ref_id = $this->getRefId();
             $data = [$ilTree->getNodeData($ref_id)]; // starts tree from root
 
-            $data = array_filter($data, fn ($item) => $this->rbac->system()->checkAccess('visible', (int) $item['ref_id']));
-
+            $data = array_filter(
+                $data,
+                fn($item) => $this->rbac->system()->checkAccess('visible', (int) $item['ref_id'])
+            );
         } else {
             $do_async = true;
             $data = $ilTree->getChildsByTypeFilter($ref_id, self::ALLOWED_TYPES);
 
-            $data = array_filter($data, fn ($item) => $this->rbac->system()->checkAccess('visible', (int) $item['ref_id']));
+            $data = array_filter(
+                $data,
+                fn($item) => $this->rbac->system()->checkAccess('visible', (int) $item['ref_id'])
+            );
 
             if (count($data) === 0) {
                 return '';
@@ -227,27 +222,27 @@ class LinkInput
                 $ref_id = $record['ref_id'];
                 $label = $record['title'] . ' (' . $record['type'] . ', ' . $ref_id . ')';
                 $icon = $environment['icon_factory']->standard($record["type"], '');
-                $url = $this->getAsyncURL($environment, (string)$ref_id);
+                $url = $this->getAsyncURL($environment, (string) $ref_id);
 
                 $obj_id = ilObject::_lookupObjId((int) $ref_id);
                 $title = ilObject::_lookupTitle($obj_id);
 
                 /** @var Node $node */
                 $node = $factory->simple($label, $icon)
-                                ->withAdditionalOnLoadCode(function ($id) use ($ref_id, $title) {
-                                    return <<<JS
+                    ->withAdditionalOnLoadCode(function ($id) use ($ref_id, $title) {
+                        return <<<JS
                         (function() {
                             const node = document.getElementById('$id');
                             const node_label = node.firstElementChild.firstElementChild;
                             node_label.addEventListener('click', () => {
                                 const ilias_link_info_element = document.getElementById('ilias_link_info');
                                 ilias_link_info_element.innerHTML = '<b>$title</b><br>Ref ID: $ref_id<br>';
-                                const link_input_element = document.getElementById('link_input_element');
+                                const link_input_element = document.querySelector('#link_input_element input');
                                 link_input_element.value = $ref_id;
                             });
                         })();
                         JS;
-                                });
+                    });
 
                 $has_children = count($this->tree->getChildsByTypeFilter((int) $ref_id, self::ALLOWED_TYPES)) > 0;
                 if ($has_children) {
@@ -258,7 +253,7 @@ class LinkInput
             }
 
             /**
-             * @param $environment
+             * @param        $environment
              * @param string $ref_id
              * @return string
              */
@@ -284,10 +279,10 @@ class LinkInput
         ];
 
         $tree = $this->factory->tree()->expandable("Label", $recursion)
-                              ->withEnvironment($environment)
-                              ->withData($data);
+            ->withEnvironment($environment)
+            ->withData($data);
 
-        if (! $do_async) {
+        if (!$do_async) {
             return $this->renderer->render($tree);
         } else {
             echo $this->renderer->renderAsync($tree->withIsSubTree(true));
